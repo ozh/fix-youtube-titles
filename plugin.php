cc41b753b6ccf25f22bca8c2766498d3426e6385
྅༻ Ǭɀħ ༄༆ཉ <ozh@ozh.org>
2021-05-30 19:01:06 +0200

    Initial release

--- plugin.php
+++ plugin.php
@@ -1,26 +1,38 @@
 <?php
 /*
-Plugin Name: Your Plugin Name
-Plugin URI: https://github.com/your_name/your_plugin
-Description: One line description of your plugin
+Plugin Name: Fix Youtube titles
+Plugin URI: https://github.com/ozh/fix-youtube-titles/
+Description: Get correct Youtube video titles
 Version: 1.0
-Author: Your Name
-Author URI: https://your-site-if-any/
+Author: Ozh
+Author URI: http://ozh.org/
 */
 
 // No direct call
 if( !defined( 'YOURLS_ABSPATH' ) ) die();
 
-/*
+// Add filters if we're trying to get a page title from Youtube
+yourls_add_filter('shunt_get_remote_title', 'ozh_is_youtube_url');
+function ozh_is_youtube_url($false, $url) {
+    // is this a youtube.com URL ?
+    if( strpos($url, 'youtube.com') > 0 ) {
+        // change user agent to "curl". Actually, this is enough to trick Youtube ¯\_(ツ)_/¯
+        yourls_add_filter('http_user_agent', function(){return 'curl/7.68.0';});
 
- Your code goes here.
- 
- Suggested read:
- https://github.com/YOURLS/YOURLS/wiki/Plugins
- https://github.com/YOURLS/YOURLS/wiki/How-to-make-Plugins
- https://github.com/YOURLS/YOURLS/wiki/Coding-Standards
- https://github.com/YOURLS/YOURLS/wiki#for-developpers
- 
- Have fun!
- 
-*/
+        // The <title> tag in Youtube pages is buried down the page : lift limit of bytes retrieved
+        yourls_add_filter('get_remote_title_max_byte', function(){return '';});
+
+        // Extra trick : fake the cookie that Youtube expects (requires YOURLS 1.8.2+)
+        yourls_add_filter('http_request_headers', 'ozh_youtube_add_consent');
+    }
+
+    // We don't want to actually interrupt the flow of event so we return false
+    return $false;
+}
+
+// Add a cookie header to the HTTP request
+function ozh_youtube_add_consent($in) {
+    $in['Cookie']='CONSENT=YES+1337';
+    return $in;
+}
+
